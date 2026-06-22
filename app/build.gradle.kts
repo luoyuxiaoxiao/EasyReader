d4f5629b50cd6bef0d2c502be33d8f63652f7f2e
luoyu <2917434804@qq.com>
2026-06-22 15:07:29 +0800

    chore: release EasyReader 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.luoyuxiaoxiao.easyreader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
